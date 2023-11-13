ca9b420c02716fa41106e3d5759487886049bd85
yuma juliana <150018196+yumajuliana@users.noreply.github.com>
2023-11-13 19:49:33 +0700

    Add files via upload

---
+++ constractor.php
@@ -0,0 +1,10 @@
+<?php
+
+// import data/person.php
+require_once "data/Person.php";
+
+// buat object new person dengan 2 parameter
+$yuma = new Person("yuma ", "mukomuko");
+
+// vardump object
+var_dump($yuma);
