9fcd4b29b4854501ec861b5fb69eccfe1242e987
yuma juliana <150018196+yumajuliana@users.noreply.github.com>
2023-11-13 19:49:11 +0700

    Add files via upload

---
+++ desturctor.php
@@ -0,0 +1,11 @@
+<?php
+
+// import data/person.php
+require_once "data/Person.php";
+
+// buat 2 object new peson dengan parameter yang berbeda
+$yuma = new Person("yuma juliana", "mukomuko");
+$yuma = new Person("yuma", "bengkulu");
+
+// tambahkan echo "Program Selesai" . PHP_EOL;
+echo "Program Selesai" . PHP_EOL;
